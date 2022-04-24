e2e33e53410cbafbeaac0c0679382c9cb1bec44f
Andrew Kaster <andrewdkaster@gmail.com>
2022-04-24 16:40:15 -0600

    serenity: Set defaults for PIE, TLS model, and panic strategy

--- compiler/rustc_target/src/spec/serenity_base.rs
+++ compiler/rustc_target/src/spec/serenity_base.rs
@@ -1,10 +1,13 @@
-use crate::spec::{cvs, RelroLevel, TargetOptions};
+use crate::spec::{cvs, PanicStrategy, RelroLevel, TargetOptions, TlsModel};
 
 pub fn opts() -> TargetOptions {
     TargetOptions {
         os: "serenity".into(),
         dynamic_linking: true,
         executables: true,
+        tls_model: TlsModel::InitialExec,
+        position_independent_executables: true,
+        panic_strategy: PanicStrategy::Abort,
         families: cvs!["unix"],
         relro_level: RelroLevel::Full,
         ..Default::default()
